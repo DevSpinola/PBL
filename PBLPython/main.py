f5c2292c539ca94521c8151a95a0db214a60a5c8
Guilherme  Santos <guilherme.santos@quaestum.com.br>
2023-11-30 12:54:52 -0300

    Tratamento de Erros

--- PBLPython/main.py
+++ PBLPython/main.py
@@ -4,16 +4,18 @@ projeteis = getProjeteis()
 meteoros = getMeteoros()
 colisoes = getColisoes()
 for colisao in colisoes:
-    velocidade_inicial = colisao.get('voParaColidir')
-    projetilEncontrado = next((projetil for projetil in projeteis if projetil.get('projetilId') == colisao.get('projetilId')), None)
-    anguloRad = projetilEncontrado.get('anguloRad')
-    tempo_total = colisao.get('tempoColisao')
-    intervalo_tempo = tempo_total/100
-    meteoroEncontrado = next((meteoro for meteoro in meteoros if meteoro.get('meteoroId') == colisao.get('meteoroId')), None)
-    distancia_meteoro = meteoroEncontrado.get('distanciaHorizontal')  # em metros
-    altura_meteoro = meteoroEncontrado.get('alturaInicial')  # em metros
-    velocidade_meteoro = meteoroEncontrado.get('velocidadeMeteoro')  # em metros por segundo
-    nome_arquivo = 'colisao' + str(colisao.get('colisaoId'))
-    print(f'Salvando animação...')
-    simulacao_animada(velocidade_inicial, anguloRad, tempo_total, intervalo_tempo, distancia_meteoro, altura_meteoro, velocidade_meteoro,nome_arquivo )
-    
+    try:
+        velocidade_inicial = colisao.get('voParaColidir')
+        projetilEncontrado = next((projetil for projetil in projeteis if projetil.get('projetilId') == colisao.get('projetilId')), None)
+        anguloRad = projetilEncontrado.get('anguloRad')
+        tempo_total = colisao.get('tempoColisao')
+        intervalo_tempo = tempo_total/100
+        meteoroEncontrado = next((meteoro for meteoro in meteoros if meteoro.get('meteoroId') == colisao.get('meteoroId')), None)
+        distancia_meteoro = meteoroEncontrado.get('distanciaHorizontal')  # em metros
+        altura_meteoro = meteoroEncontrado.get('alturaInicial')  # em metros
+        velocidade_meteoro = meteoroEncontrado.get('velocidadeMeteoro')  # em metros por segundo
+        nome_arquivo = 'colisao' + str(colisao.get('colisaoId'))
+        print(f'Salvando animação...')
+        simulacao_animada(velocidade_inicial, anguloRad, tempo_total, intervalo_tempo, distancia_meteoro, altura_meteoro, velocidade_meteoro,nome_arquivo )
+    except Exception as e :
+        print(e)
